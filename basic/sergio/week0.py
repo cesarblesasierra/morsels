db1e7ccd26a61ce4bfa7515e308a7ad60ae7c803
Sergio Marrero <sergiomarreromarrero@gmail.com>
2020-03-05 18:19:20 +0100

    Adda bonus 2 to week0

--- basic/sergio/week0.py
+++ basic/sergio/week0.py
@@ -37,5 +37,36 @@ matrix3 = [[2, 3], [-3, 1]]
 assert add(matrix1, matrix2, matrix3) == [[8, 8], [7, 7]]
 
 # Bonus 2
+# Bonus 2
+def add(*args):
+    # Check dimensions are correct
+    try:
+        if all([len(matrix) == len(args[0]) for matrix in args]) == False:
+            raise ValueError
+    except ValueError as v_error:
+        print("Given matrices are not the same size.")
+        return v_error
 
+    for rows in zip(*args):
+        l = []
+        for row in rows:
+            l.append(len(row))
+        try:
+            if all([x == l[0] for x in l]) == False:
+                raise ValueError
+        except ValueError as v_error:
+            print("Given matrices are not the same size.")
+            return v_error
+
+    # Matrix Sum
+    matrix = []
+    for vects2sum in zip(*args):
+        vector = []
+        for elem2sum in zip(*vects2sum):
+            vector.append(sum(elem2sum))
+        matrix.append(vector)
+    return matrix
+
+
+assert isinstance(add([[1, 9], [7, 3]], [[1, 2], [3]]), ValueError) 
 
