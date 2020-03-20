65fea702a9bd9dda7c0f352e696d46fea5a7eb00
sergio <sergio.marrero@col.vueling.com>
2020-03-20 13:20:49 +0100

    Bonus 3

--- users/sergio/week1.py
+++ users/sergio/week1.py
@@ -23,8 +23,9 @@
 # 2
 # There are three bonuses for this exercise.
 
+#################################
 # Base exercise
-
+#################################
 import numpy as np
 from math import pi
 class Circle:
@@ -48,10 +49,10 @@ def base__battery_of_tests():
 
 base__battery_of_tests()
 
-# Bonus 1
-# Base exercise
-# Properties
 
+
+#################################
+# Bonus 1
 #################################
 # Using Property Function
 class Circle:
@@ -127,137 +128,65 @@ bonus1__battery_of_tests()
 
 
 
-## Bonus 2
-c = Circle(2)
-c.radius = 1
-c.diameter
-np.testing.assert_approx_equal(c.area, 3.141592653589793, significant=3)
-print(c)
-
-
-
-
-
-
-
-################################3
-class Circle:
-
-    def __init__(self, radius=1):
-        self.__radius=radius
-        self.__diameter = radius*2
-        self.__area = pi*radius**2
-
-    def __set_radius(self, radius):
-        self.__radius = radius
-        self.__diameter = 2*radius
-        self.__area = pi*radius**2
-
-    def __get_radius(self):
-        return self.__radius
-
-    def __set_diameter(self, diameter):
-        self.__diameter = diameter
-        self.__radius = diameter/2
-        self.__area = pi*(diameter/2)**2
-
-    def __get_diameter(self):
-        return self.__diameter
-
-    def __get_area(self):
-        return self.__area
-
-    radius = property(fset=__set_radius, fget=__get_radius, doc="My radius")
-    diameter = property(fset=__set_diameter, fget=__get_diameter, doc="My diameter")
-    area = property(fget=__get_area, doc="My area")
-
-    def __str__(self):
-        return f'radius: {self.__radius}\ndiameter: {self.__diameter}\narea: {self.__area}'.format(self=self)
-
-
-def main():
-    c=Circle(2)
-    c.radius = 1
-    assert c.radius == 1, 'Diameter is wrong'
-    assert c.diameter == 2, 'Diameter is wrong'
-    assert c.area == 3.14, 'Area is wrong'
-    print('Test passed with success!!')
-
-main()
-
-
+#################################
 # Bonus 2
-# With decorators
-# Properties
+#################################
 class Circle:
 
     def __init__(self, radius=1):
-        self.__set_radius(radius)
-
-
-    def __set_radius(self, radius):
-
-        if radius < 0:
-            raise ValueError("Radius cannot be negative")
-
-        self.__radius = radius
-        self.__diameter = 2*radius
-        self.__area = pi*radius**2
-
-
-
-    def __get_radius(self):
-        return self.__radius
-
-    def __set_diameter(self, diameter):
-        self.__diameter = diameter
-        self.__set_radius(diameter/2)
-        self.__area = pi*(diameter/2)**2
-
-    def __get_diameter(self):
-        return self.__diameter
-
-    def __get_area(self):
-        return self.__area
-
-    radius = property(fset=__set_radius, fget=__get_radius, doc="My radius")
-    diameter = property(fset=__set_diameter, fget=__get_diameter, doc="My diameter")
-    area = property(fget=__get_area, doc="My area")
-
-    def __str__(self):
-        return f'radius: {self.__radius}\ndiameter: {self.__diameter}\narea: {self.__area}'.format(self=self)
-
-c=Circle(2)
-c.radius = 1
-c.diameter = -1
-c.radius
-
-
-def main():
-    c=Circle(2)
-    c.radius = 1
-    assert c.radius == 1, 'Diameter is wrong'
-    assert c.diameter == 2, 'Diameter is wrong'
-    assert c.area == 3.14, 'Area is wrong'
-    print('Test passed with success!!')
-
-main()
-
-
-
-# Con properties
-class Circle:
-
-    def __init__(self, radius=1):
-        self.__radius = radius
+        self.radius=radius
 
     @property
-    def __radius(self):
+    def radius(self):
         return self.__radius
 
-    @__radius.setter
-    def __radius(self, radius):
+    @radius.setter
+    def radius(self, radius):
+        self.__radius = radius
+        self.__diameter = 2*radius
+        self.__area = pi*radius**2
 
+    @property
+    def diameter(self):
+        return self.__diameter
+
+    @diameter.setter
+    def diameter(self, diameter):
+
+        self.__diameter = diameter
+        self.__radius = diameter/2
+        self.__area = pi*self.__radius**2
+
+
+    @property
+    def area(self):
+        return self.__area
+
+    def __str__(self):
+        return f'radius: {self.radius}\ndiameter: {self.diameter}\narea: {self.area}'.format(self=self)
+
+c=Circle()
+print(c)
+c.diameter=4
+print(c)
+c.area =4
+
+
+
+#################################
+# Bonus 3
+#################################
+class Circle:
+
+    def __init__(self, radius=1):
+        self.radius=radius
+
+    @property
+    def radius(self):
+        return self.__radius
+
+    @radius.setter
+    def radius(self, radius):
         if radius < 0:
             raise ValueError("Radius cannot be negative")
 
@@ -265,37 +194,26 @@ class Circle:
         self.__diameter = 2*radius
         self.__area = pi*radius**2
 
-
-    def __set_diameter(self, diameter):
-        self.__diameter = diameter
-        self.__set_radius(diameter/2)
-        self.__area = pi*(diameter/2)**2
-
-    def __get_diameter(self):
+    @property
+    def diameter(self):
         return self.__diameter
 
-    def __get_area(self):
+    @diameter.setter
+    def diameter(self, diameter):
+        self.radius = diameter/2
+
+
+    @property
+    def area(self):
         return self.__area
 
-    radius = property(fset=__set_radius, fget=__get_radius, doc="My radius")
-    diameter = property(fset=__set_diameter, fget=__get_diameter, doc="My diameter")
-    area = property(fget=__get_area, doc="My area")
-
     def __str__(self):
-        return f'radius: {self.__radius}\ndiameter: {self.__diameter}\narea: {self.__area}'.format(self=self)
+        return f'radius: {self.radius}\ndiameter: {self.diameter}\narea: {self.area}'.format(self=self)
 
-
-
-
-def my_decorator(func):
-    def wrapper():
-        print("Something is happening before the function is called.")
-        func()
-        print("Something is happening after the function is called.")
-    return wrapper
-
-def say_whee():
-    print("Whee!")
-
-say_whee = my_decorator(say_whee)
-say_whee
+c=Circle()
+print(c)
+c.diameter=4
+print(c)
+c.area =4
+c.radius=-1
+c.diameter=-1
