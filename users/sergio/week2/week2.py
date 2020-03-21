e8c00192fe2f8656d6586814cadddfe52e9eb690
sergio <sergio.marrero@col.vueling.com>
2020-03-21 23:04:54 +0100

    changes done in the week 2 base exercise

--- users/sergio/week2/week2.py
+++ users/sergio/week2/week2.py
@@ -3,34 +3,34 @@ import csv
 from pathlib import Path
 import sys
 
-def custom_read_csv(*args):
 
-    pathtocsv = Path('data', args[0])
-    l = []
-    csv.register_dialect('pipes', delimiter='|')
-    with open(pathtocsv, 'rt') as f:
-        reader = csv.reader(f, dialect='pipes')
+def custom_read_csv(*args, **kwargs):
+    read_csv_path = Path('data', args[0])
+    with open(read_csv_path, 'r') as file_to_read:
+        reader = csv.reader(file_to_read, delimiter=',')
+        if not kwargs.get('header', True):
+            next(reader)
         for row in reader:
-            l.append(row)
-    return l
+            print(row)
 
 
-def custom_write_csv(*args):
-    path_to_csv = Path('data', args[0])
-    with open(path_to_csv, 'wt') as f:
-        writer = csv.writer(f, lineterminator='\n')
-        for row in args[1]:
-            writer.writerow(row)
+
+def transform_csv(*args, **kwargs):
+
+    read_csv_path = Path('data', args[0])
+    write_csv_path = Path('data', args[1])
+    with open(read_csv_path, 'r') as file_to_read:
+        reader = csv.reader(file_to_read, delimiter='|')
+        with open(write_csv_path, 'w') as file_to_write:
+            writer = csv.writer(file_to_write, delimiter=',')
+            if not kwargs.get('header', True):
+                next(reader)
+            for row_readed in reader:
+                writer.writerow(row_readed)
+
 
 
 
 if __name__ == "__main__":
-
-    list_csv_file = custom_read_csv(sys.argv[1])
-    custom_write_csv(sys.argv[2], list_csv_file)
-
-    print('Original:')
-    print(list_csv_file)
-    print('#################')
-    print('Formatted')
-    print("\n".join(str(row) for row in custom_read_csv(sys.argv[2])))
+    transform_csv(sys.argv[1], sys.argv[2], header=True)
+    custom_read_csv(sys.argv[2], header=True)
